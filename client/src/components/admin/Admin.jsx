6f2cf9cebab84553d29dc15996f9814e4dd927c8
Mikaelisson <mikaelissongesuino@gmail.com>
2022-12-12 23:58:16 -0300

    Page admin front end

--- client/src/components/admin/Admin.jsx
+++ client/src/components/admin/Admin.jsx
@@ -1,27 +1,35 @@
 import GlobalStyled from "../styles/global";
-import { ButtonsForm, Container, FormLogin, InputGroup } from "./AdminStyled";
+import {
+  BorderEffect,
+  ButtonsForm,
+  Container,
+  FormLogin,
+  InputGroup,
+} from "./AdminStyled";
 
 const Admin = () => {
   return (
     <Container>
-      <FormLogin>
-        <h1>Entre com sua conta</h1>
-        
-        <InputGroup>
-          <input type="text" name="email" />
-          <label htmlFor="email">Email</label>
-        </InputGroup>
+      <BorderEffect>
+        <FormLogin>
+          <h1>Entre com sua conta</h1>
 
-        <InputGroup>
-          <input type="text" name="password" />
-          <label htmlFor="password">Senha</label>
-        </InputGroup>
+          <InputGroup>
+            <input type="text" name="email" />
+            <label htmlFor="email">Email</label>
+          </InputGroup>
 
-        <ButtonsForm>
-          <button type="button">Cancelar</button>
-          <button type="submit">Entrar</button>
-        </ButtonsForm>
-      </FormLogin>
+          <InputGroup>
+            <input type="text" name="password" />
+            <label htmlFor="password">Senha</label>
+          </InputGroup>
+
+          <ButtonsForm>
+            <button type="button">Cancelar</button>
+            <button type="submit">Entrar</button>
+          </ButtonsForm>
+        </FormLogin>
+      </BorderEffect>
       <GlobalStyled />
     </Container>
   );
