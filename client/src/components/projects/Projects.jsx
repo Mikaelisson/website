c2ce2055d0016a092b04b31bc018ba373eac7801
Mikaelisson <mikaelissongesuino@gmail.com>
2023-05-31 14:40:02 -0300

    Adjustment in the position of projects

--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -33,45 +33,50 @@ const Projects = (props) => {
 
       <div>
         {loadProjects ? (
-          projects.map((element, index) => {
-            return (
-              <Project key={index}>
-                <ImageProject>
-                  <img src={element.image} alt={`Imagem de ${element.name}`} />
-                </ImageProject>
-                <ProjectData>
-                  {element.title && <h3>{element.title}</h3>}
-                  {element.description && <p>{element.description}</p>}
-                  {element.comments && <p>{element.comments}</p>}
-                  {element.mobileSupport ? (
-                    <p>Com suporte para mobile</p>
-                  ) : (
-                    <p>Sem suporte para mobile</p>
-                  )}
-                </ProjectData>
-                <ContainerButtons>
-                  <LinkProject
-                    widthElement="auto"
-                    href={element.repository}
-                    target="_blank"
-                  >
-                    <FaGithub />
-                  </LinkProject>
-                  <LinkProject
-                    widthElement="100%"
-                    href={element.url}
-                    target="_blank"
-                  >
-                    <div>
-                      ver mais
-                      <BiLinkExternal />
-                    </div>
-                  </LinkProject>
-                  {props.children}
-                </ContainerButtons>
-              </Project>
-            );
-          })
+          projects
+            .map((element, index) => {
+              return (
+                <Project key={index}>
+                  <ImageProject>
+                    <img
+                      src={element.image}
+                      alt={`Imagem de ${element.name}`}
+                    />
+                  </ImageProject>
+                  <ProjectData>
+                    {element.title && <h3>{element.title}</h3>}
+                    {element.description && <p>{element.description}</p>}
+                    {element.comments && <p>{element.comments}</p>}
+                    {element.mobileSupport ? (
+                      <p>Com suporte para mobile</p>
+                    ) : (
+                      <p>Sem suporte para mobile</p>
+                    )}
+                  </ProjectData>
+                  <ContainerButtons>
+                    <LinkProject
+                      widthElement="auto"
+                      href={element.repository}
+                      target="_blank"
+                    >
+                      <FaGithub />
+                    </LinkProject>
+                    <LinkProject
+                      widthElement="100%"
+                      href={element.url}
+                      target="_blank"
+                    >
+                      <div>
+                        ver mais
+                        <BiLinkExternal />
+                      </div>
+                    </LinkProject>
+                    {props.children}
+                  </ContainerButtons>
+                </Project>
+              );
+            })
+            .reverse()
         ) : (
           <LoadingSimple />
         )}
