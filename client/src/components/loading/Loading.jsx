e5f0de45bfd98d785eaa78389cdadaac032a1976
Mikaelisson <mikaelissongesuino@gmail.com>
2023-05-31 13:29:07 -0300

    Adjustment loading

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -8,6 +8,9 @@ const LoadingStyled = styled.div`
   top: 0;
   left: 0;
   z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   ::after,
   ::before {
@@ -20,9 +23,9 @@ const LoadingStyled = styled.div`
     border-radius: 100%;
 
     position: absolute;
-    top: 50%;
+    /* top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%); */
 
     animation: load 2s infinite;
   }
@@ -38,9 +41,48 @@ const LoadingStyled = styled.div`
     }
   }
 `;
+const LoadingSimpleStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+
+  ::after,
+  ::before {
+    content: "";
+    width: 50px;
+    height: 50px;
+
+    border: transparent solid 4px;
+    border-right: #e1ac04 solid 4px;
+    border-radius: 100%;
+
+    position: absolute;
+
+    animation: load 2s infinite;
+  }
+  ::before {
+    animation-delay: 0.8s;
+  }
+  @keyframes load {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 const Loading = () => {
   return <LoadingStyled />;
+};
+
+export const LoadingSimple = () => {
+  return <LoadingSimpleStyled />;
 };
 
 export default Loading;
--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -12,15 +12,18 @@ import { Title } from "../styles/MainStyled";
 
 import { FaGithub } from "react-icons/fa";
 import { BiLinkExternal } from "react-icons/bi";
+import { LoadingSimple } from "../loading/Loading";
 
 const Projects = (props) => {
   const [projects, setProjects] = useState([]);
+  const [loadProjects, setLoadProjects] = useState(false);
 
   useEffect(() => {
     fetch("/api/search/projects")
       .then((res) => res.json())
       .then((res) => {
         setProjects(res);
+        setLoadProjects(true);
       });
   }, []);
 
@@ -29,45 +32,49 @@ const Projects = (props) => {
       <Title>projetos</Title>
 
       <div>
-        {projects.map((element, index) => {
-          return (
-            <Project key={index}>
-              <ImageProject>
-                <img src={element.image} alt={`Imagem de ${element.name}`} />
-              </ImageProject>
-              <ProjectData>
-                {element.title && <h3>{element.title}</h3>}
-                {element.description && <p>{element.description}</p>}
-                {element.comments && <p>{element.comments}</p>}
-                {element.mobileSupport ? (
-                  <p>Com suporte para mobile</p>
-                ) : (
-                  <p>Sem suporte para mobile</p>
-                )}
-              </ProjectData>
-              <ContainerButtons>
-                <LinkProject
-                  widthElement="auto"
-                  href={element.repository}
-                  target="_blank"
-                >
-                  <FaGithub />
-                </LinkProject>
-                <LinkProject
-                  widthElement="100%"
-                  href={element.url}
-                  target="_blank"
-                >
-                  <div>
-                    ver mais
-                    <BiLinkExternal />
-                  </div>
-                </LinkProject>
-                {props.children}
-              </ContainerButtons>
-            </Project>
-          );
-        })}
+        {loadProjects ? (
+          projects.map((element, index) => {
+            return (
+              <Project key={index}>
+                <ImageProject>
+                  <img src={element.image} alt={`Imagem de ${element.name}`} />
+                </ImageProject>
+                <ProjectData>
+                  {element.title && <h3>{element.title}</h3>}
+                  {element.description && <p>{element.description}</p>}
+                  {element.comments && <p>{element.comments}</p>}
+                  {element.mobileSupport ? (
+                    <p>Com suporte para mobile</p>
+                  ) : (
+                    <p>Sem suporte para mobile</p>
+                  )}
+                </ProjectData>
+                <ContainerButtons>
+                  <LinkProject
+                    widthElement="auto"
+                    href={element.repository}
+                    target="_blank"
+                  >
+                    <FaGithub />
+                  </LinkProject>
+                  <LinkProject
+                    widthElement="100%"
+                    href={element.url}
+                    target="_blank"
+                  >
+                    <div>
+                      ver mais
+                      <BiLinkExternal />
+                    </div>
+                  </LinkProject>
+                  {props.children}
+                </ContainerButtons>
+              </Project>
+            );
+          })
+        ) : (
+          <LoadingSimple />
+        )}
       </div>
     </ContainerProjects>
   );
