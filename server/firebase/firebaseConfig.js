0d30508822db4f8d1d260600790053e0c0eb0f37
Mikaelisson <mikaelissongesuino@gmail.com>
2023-06-05 19:32:04 -0300

    Adjustment variables

--- server/firebase/firebaseConfig.js
+++ server/firebase/firebaseConfig.js
@@ -4,8 +4,21 @@ const BUCKET = "portfolio-278b8.appspot.com";
 var serviceAccount;
 
 if (process.env.NODE_ENV != "development") {
-  serviceAccount = require("../../firebase-key.json");
+  //deploy
+  serviceAccount = {
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+  };
 } else {
+  //localhost
   serviceAccount = require("./firebase-key.json");
 }
 
